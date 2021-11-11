0aa25f2c1bc8f3fd7f073cb62b8e03d9afbfc111
Xkpd <87077708+Xkpd@users.noreply.github.com>
2021-11-12 02:55:19 +0800

    Added java doc

--- src/SetPackage.java
+++ src/SetPackage.java
@@ -1,29 +1,49 @@
 package src;
-/**
- * SetPackage
- */
-//if we calling the objects from flat file.... 
-//technically we do not need to inherit but let's see how this goes if it throws an error we might have to cancel inheritance
-//cuz in flat file we have an array of items, so we need to parse it and then get the total price 
-//DO NOT USE NAME TO DO anything
-//always use ID because ID is unique 
 
 //item id is SP<no.>
 //SP stands for set package
+
+	/**
+	 * Combines specific items in the menu into set packages.
+	 * A set package can have many item in the menu.
+	 */
 public class SetPackage extends MenuItems {
 
+	/**
+	 * Indicates the availability of promotion for the set package.
+	 */
 	private boolean Promo;
+	/**
+	 * List of item in the set package.
+	 */
 	private MenuItems[] SetList;
 	
+	/**
+	 * Gets the list of item in this set package.
+	 * @return This Set Package's list of item.
+	 */
 	public MenuItems[] getSetList(){
 		return this.SetList;
 	}
 
+	/**
+	 * Changes the list of item and price for this set package.
+	 * @param NewSetList This Set Package's new list.
+	 */
 	public void setSetList(MenuItems[] NewSetList){
 		this.SetList = NewSetList;
 		this.setPrice(calculatePrice(NewSetList));
 	}
 
+	/**
+	 * Creates a new set package will details required. 
+	 * New set package should not be the same as other set package.
+	 * @param itemID This Set Package's item ID.
+	 * @param SetList This Set Package's list of items.
+	 * @param Name This Set Package's name.
+	 * @param promo This Set Package's promotional availability.
+	 * @param Description This Set Package's description.
+	 */
 	public SetPackage(String itemID, MenuItems[] SetList, String Name,boolean promo, String Description) {
 		super(itemID,Name,"Set Item",(float)0,Description);
 		this.setPromo(promo);
@@ -32,6 +52,11 @@ public class SetPackage extends MenuItems {
 	}
 
 
+	/**
+	 * Calculates the total price of item in the set package with promotion (if available).
+	 * @param SetList This Set Package's list of item.
+	 * @return This Set Package's total price.
+	 */
 	protected float calculatePrice(MenuItems[] SetList) {
 		float price = (float) 0;
 		int i;
@@ -45,10 +70,18 @@ public class SetPackage extends MenuItems {
 		return price;
 	}
 
+	/**
+	 * Gets this set package's promotional status.
+	 * @return This Set Package's promotional status.
+	 */
 	public boolean getPromo() {
 		return this.Promo;
 	}
 
+	/**
+	 * Changes this set package's promotional status.
+	 * @param promo This Set Package's promotional status. 
+	 */
 	public void setPromo(boolean promo) {
 		this.Promo = promo;
 	}
