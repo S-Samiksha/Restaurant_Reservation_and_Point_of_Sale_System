e70aa7d9aad8a3d32d3fd275b850d0f65a7765a7
Xkpd <87077708+Xkpd@users.noreply.github.com>
2021-11-13 01:50:25 +0800

    Updated with java doc

--- src/Staff.java
+++ src/Staff.java
@@ -2,16 +2,53 @@ package src;
 
 //this one also, parse using flat file 
 // Honestly do not need set functions but we should discuss this!
-
+/**
+ * Represents a staff in the restaurant.
+ * A restaurant can have many staff.
+ * A staff will be assigned to an order and reservation.
+ */
 public class Staff {
+	/**
+	 * Full name of this staff.
+	 */
 	private String Name;
+	/**
+	 * Gender of this staff.
+	 */
 	private String Gender;
+	/**
+	 * ID of this staff.
+	 */
 	private String EmployeeID;
+	/**
+	 * Contact number of this staff.
+	 */
 	private int ContactNumber;
+	/**
+	 * Job title of this staff.
+	 */
 	private String JobTitle;  //this is needed but not inside the class diagram 
+	/**
+	 * Availability of this staff to server new customer.
+	 */
 	private boolean isAvailable;
 
-
+	/**
+	 * Creates a new staff with details.
+	 * Legal process is required to sign contract with this new staff.
+	 * @param EmployeeID This Staff's employee ID.
+	 * @param Name This Staff's name.
+	 * 				Should include full name.
+	 * 				Should not be changed after staff enrollment.
+	 * @param Gender This Staff's gender.
+	 * 					Should not be changed after staff enrollment.
+	 * @param ContactNumber This Staff's contact number.
+	 * 						Should not be changed after staff enrollment.
+	 * @param JobTitle This Staff's job title.
+	 * 					Must be either waiter or manager.
+	 * @param isAvailaible This Staff's availability.
+	 * 						Staff will always be initialized as available.
+	 */
 	public Staff(String EmployeeID, String Name, String Gender, int ContactNumber, String JobTitle, boolean isAvailaible){
 		this.Name = Name;
 		this.Gender = Gender;
@@ -21,21 +58,28 @@ public class Staff {
 		this.isAvailable = isAvailaible;
 	}
 
+	/**
+	 * Gets the full name of this staff.
+	 * @return This Staff's full name.
+	 */
 	public String getName() {
 		//throw new UnsupportedOperationException();
 		return this.Name;
 	}
 
-	/**
-	 * 
-	 * @param String
+	/**Gets the gender of this staff.
+	 * @param String This Staff's gender.
 	 */
-
 	public String getGender() {
 		//throw new UnsupportedOperationException();
 		return this.Gender;
 	}
 
+	/**
+	 * Gets the availability of this staff.
+	 * @return This Staff's availability.
+	 * 			Only able to serve new customer when staff is available.
+	 */
 	public boolean getisAvailable(){
 		return this.isAvailable;
 	}
@@ -66,21 +110,26 @@ public class Staff {
 
 	}*/
 
+	/**
+	 * Gets the ID of this staff.
+	 * @return This Staff's employee ID.
+	 */
 	public String getEmployeeID() {
 		// throw new UnsupportedOperationException();
 		return this.EmployeeID;
 	}
 
 
+	/**
+	 * Gets the contact number of this staff.
+	 * @return This Staff's contact number.
+	 */
 	public int getContactNumber() {
 		// throw new UnsupportedOperationException();
 		return this.ContactNumber;
 	}
 
-	/**
-	 * 
-	 * @param Double
-	 */
+
 	/*protected void setContactNumber(int ContactNumber) { //this one also dont need 
 		// throw new UnsupportedOperationException();
 		//check if the string length is 8 digits or not, and valid (in terms of singapore number)
@@ -99,7 +148,11 @@ public class Staff {
 
 	}*/
 
-
+	/**
+	 * Checks is this staff is manage.
+	 * @return If this Staff is manager
+	 * 			Return true if is manager and false if is not.
+	 */
 	public boolean isManager(){
 		if (this.JobTitle.equals("Manager")){
 			return true;
@@ -107,7 +160,11 @@ public class Staff {
 			return false; 
 		}
 	}
-
+	
+	/**
+	 * Changes the availability of this staff.
+	 * @param Available This Staff's availability.
+	 */
 	public void setisAvailable(boolean available){
 		this.isAvailable = available;
 	}
