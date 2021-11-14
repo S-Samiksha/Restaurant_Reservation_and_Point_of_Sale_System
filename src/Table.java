35b3d69f555c6098b4ab273ee725823295acb8e9
Xkpd <87077708+Xkpd@users.noreply.github.com>
2021-11-14 18:05:42 +0800

    Update Table.java

--- src/Table.java
+++ src/Table.java
@@ -1,25 +1,9 @@
 package src;
-/**
- * Table 
- * 
- * this one also needs flat file  --> create a constructor 
-
- ---
- table flat file will be better if it is sorted ->  more efficient 
- I alr sorted it but just make sure-> whoever is doing this 
- reason why:
- so for example we have a pax of 4 -> ideally we should give them a four seater table.
- But then imagine our first available table as we iterate through table.txt is 6. 
- Then its not so efficient.
-
- - shreya xoxo <3
- ---
- * 
- */
 
 /**
  * Represents a table in the restaurant. 
  * A restaurant can have many tables.
+ * A table can only have 1 order or 1 reservation and 1 staff.
  */
 
 public class Table {
@@ -27,7 +11,7 @@ public class Table {
 	/**
 	 * Availability of the table.
 	 */
-	private boolean isAvailable = false; // initialize to 0
+	private boolean isAvailable = false; 
 	/**
 	 * Maximum capacity of the table.
 	 */
@@ -36,19 +20,6 @@ public class Table {
 	 * Specific number/ID given to the table.
 	 */
 	private int tableNum;
-	/*
-	max capacity = 10 (in the manual)
-	min capacity = 2 (in the manual)
-	capacity = even (in the manual)
-	setTableCapacity()
-	setTableNumber()
-	cannot be inside due to security reason. Imagine.... 
-	An angry staff knows how we code.... 
-	if we can set table number... 
-	they can set everything to 0 and sabotage the restaruant so no
-	hence, we can only have get methods to mainclass
-
-	*/
 	
 	/**
 	 * Creates a new table for the restaurant.
@@ -82,8 +53,8 @@ public class Table {
 
 	/**
 	 * Gets the maximum capacity of the table.
-	 * Table cannot be assignment if the number of customer exits the maximum table capacity.
-	 * Table capacity can only be changed through security access by authorized personnel as it is fixed and to prevent staff from changing. 
+	 * Table cannot be assignment if the number of customer exceeds the maximum table capacity.
+	 * Table capacity can only be changed through security access by authorized personnel as it is fixed to prevent staff from changing. 
 	 * @return This Table's capacity.
 	 */
 	public int gettableCapacity(){
@@ -99,9 +70,4 @@ public class Table {
 		return this.tableNum;
 	}
 
-
-
-
-
-	
 }
